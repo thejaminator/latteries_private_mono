192215fc1b8dbca548c4d2ade6b20a16eaafe828
James Chua <chuajamessh@gmail.com>
2025-12-18 20:57:56 +0800

    save

--- private_scripts/subliminal_learning/test_model_compliance_numbers.py
+++ private_scripts/subliminal_learning/test_model_compliance_numbers.py
@@ -69,13 +69,13 @@ async def main(system_prompt: str | None = None):
 
     # Configure the model
     config = InferenceConfig(
-        # model="Qwen/Qwen3-32B",
+        model="Qwen/Qwen3-32B",
         # model="tinker://e48f116f-590f-51ba-9349-57e65b5cf400:train:0/sampler_weights/final",
         # 82cb0736-7875-5d45-ae98-f3ef8e99bd49:train:0 step 20
         # model="tinker://82cb0736-7875-5d45-ae98-f3ef8e99bd49:train:0/sampler_weights/000020",
         # tinker://b8e91cee-541c-5032-a5e6-6f058f0ed139:train:0/sampler_weights/000030 step 30
         # model="tinker://b8e91cee-541c-5032-a5e6-6f058f0ed139:train:0/sampler_weights/000030",
-        model="tinker://32ec03b6-18a6-5262-a0e0-472d50648cce:train:0/sampler_weights/final",
+        # model="tinker://32ec03b6-18a6-5262-a0e0-472d50648cce:train:0/sampler_weights/final",
         max_tokens=100,
         temperature=1.0,
         top_p=1.0,
@@ -95,7 +95,7 @@ async def main(system_prompt: str | None = None):
     )
 
     # Generate multiple prompts
-    num_prompts = 20
+    num_prompts = 100
     prompts = [generator.sample_query() for _ in range(num_prompts)]
 
     # Create tasks (repeat each prompt multiple times)
